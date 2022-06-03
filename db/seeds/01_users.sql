aae7e049a84f54cf3ba65ed6135adf3cd915d1bd
Minh Vo <minhvo199@gmail.com>
2022-06-03 23:33:43 +0000

    create table schema

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Minh', 'Vo', 'test@gmail.com', 'password');
