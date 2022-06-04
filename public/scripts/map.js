5f631ef4e67d824d702d4c70aa1f09b76e5bb749
Minh Vo <minhvo199@gmail.com>
2022-06-04 23:31:09 +0000

    add map markers

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,6 +1,6 @@
 // Initialize Leaflet map
 $(document).ready(function() {
-  let map = L.map('map').setView([51.505, -0.09], 13);
+  let map = L.map('map').setView([49.246292, -123.116226], 13);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
