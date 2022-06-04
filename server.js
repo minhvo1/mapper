f3ff42f4845bdc0de316cb8680dd7758d9dc0d74
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-06-03 21:01:54 -0700

    user login and logout

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require("cookie-session");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -22,7 +23,15 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_KEY1, process.env.COOKIE_KEY2],
 
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 app.use(
   "/styles",
   sassMiddleware({
