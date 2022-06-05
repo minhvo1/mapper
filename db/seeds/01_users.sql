fc6e1be431ab55acb4cd210e78267b56e8f08ef6
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-06-04 18:41:38 -0700

    add seeds sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (first_name, last_name, email, password) VALUES ('Minh', 'Vo', 'test@gmail.com', 'password');
-INSERT INTO users (first_name, last_name, email, password) VALUES ('don', 'kim', 'test1@gmail.com', 'password');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Minh', 'Vo', 'test@gmail.com', '$2a$10$EOX3Km0cyoFiJ7GzIzjJ4Oq81m9u4XhACZEIoPeXW3reBQIarf6B6');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('don', 'kim', 'test1@gmail.com', '$2a$10$EOX3Km0cyoFiJ7GzIzjJ4Oq81m9u4XhACZEIoPeXW3reBQIarf6B6');
