d18c268ec4baad7e555e2029c7081f3fb2fe2862
Minh Vo <minhvo199@gmail.com>
2022-06-06 02:18:27 +0000

    implement display points and point data for existing maps

--- public/scripts/mapInitialize.js
+++ public/scripts/mapInitialize.js
@@ -1,7 +1,7 @@
 // Initialize Leaflet map
 $(document).ready(function() {
   let map = L.map('map').setView([49.246292, -123.116226], 13);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  let tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
@@ -13,20 +13,31 @@ $(document).ready(function() {
     console.log(markersArray);
   })
 
+  //  Get map points on click
+  let marker;
+  let markers = [];
+  $(".user-maps").on("click", ".map-name", function () {
+    const key = $(this).attr("data-input");
 
-  $('.user-maps').on('click', '.map-name', function () {
-    const key = $(this).attr('data-input');
-    console.log($(this).attr('data-input'));
+    if (markers) {
+      for (let i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
+      }
+    }
     $.ajax({
-      type: 'GET',
+      type: "GET",
       url: `/api/maps/${key}`,
       success: (result) => {
-        console.log(result);
-
-      }
-
-    })
-  })
+        // console.log(result.data);
+        const points = result.data;
+        for (const point of points) {
+          marker = new L.Marker([point.lat, point.long]).bindPopup(`Title: ${point.title}; Description: ${point.description}`);
+          markers.push(marker);
+          map.addLayer(marker);
+        }
+      },
+    });
+  });
 
 });
 
